2682f877bd331ce6110aeccdbcf4adc8d81a7057
guitarbuju <gerardoamadrid@gmail.com>
2024-11-04 12:02:20 +0100

    fixed apos

--- src/app/sections/docs/page.tsx
+++ src/app/sections/docs/page.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 function page() {
   return (
-    <div className=" text-gray-50 p-4 w-3/4">
-      <h1 className="text-2xl  mb-4 text-violet-400 font-bold underline">
-        Docs:
-      </h1>
+    <div className="text-gray-50 p-4 w-3/4">
+      <h1 className="text-2xl mb-4 text-violet-400 font-bold underline">Docs:</h1>
       <div className="flex flex-col gap-4">
         <h1 className="text-xl text-violet-400 underline font-semibold">Overview:</h1>
         <p>
@@ -19,13 +17,12 @@ function page() {
         <h1 className="text-xl text-violet-400 underline font-semibold">Features:</h1>
         <ul className="list-disc">
           <li>
-            {" "}
             <p>
-              Preview & Copy Code: Each UI component is displayed within a
+              Preview &amp; Copy Code: Each UI component is displayed within a
               ShadCN Tabs component. The tabs let users switch seamlessly
               between the Preview tab to see the component in action, and the
               Code tab, where users can view, select, and copy the code to
-              customize and integrate into their own projects.{" "}
+              customize and integrate into their own projects.
             </p>
           </li>
           <li>
@@ -37,7 +34,7 @@ function page() {
           </li>
           <li>
             <p>
-              Mobile & Small Screens: On mobile and smaller screens, the sidebar
+              Mobile &amp; Small Screens: On mobile and smaller screens, the sidebar
               transforms into a top bar, ensuring a responsive and accessible
               navigation experience.
             </p>
@@ -46,10 +43,9 @@ function page() {
         <h1 className="text-xl text-violet-400 underline font-semibold">Tech Stacks:</h1>
         <ul className="list-disc">
           <li>
-            {" "}
             <p>
               Next.js: The foundation of the app, providing server-side
-              rendering, routing, and fast performance.{" "}
+              rendering, routing, and fast performance.
             </p>
           </li>
           <li>
@@ -73,9 +69,8 @@ function page() {
           </li>
         </ul>
         <h1 className="text-xl text-violet-400 underline font-semibold">Usage:</h1>
-        <ol className="list-disc">
+        <ol className="list-decimal">
           <li>
-            {" "}
             <p>
               Browsing Components: Navigate through the sidebar (or top bar on
               smaller screens) to access different UI components.
@@ -83,7 +78,7 @@ function page() {
           </li>
           <li>
             <p>
-              Preview & Code Tabs: Click on any component to open its dedicated
+              Preview &amp; Code Tabs: Click on any component to open its dedicated
               tab view, where you can toggle between the Preview and Code tabs.
             </p>
           </li>
@@ -95,11 +90,10 @@ function page() {
           </li>
         </ol>
         <h1 className="text-xl text-violet-400 underline font-semibold">Responsive Design:</h1>
-        <ol className="list-disc">
+        <ol className="list-decimal">
           <li>
-            {" "}
             <p>
-              The app's sidebar navigation automatically adapts to the screen
+              The app&apos;s sidebar navigation automatically adapts to the screen
               size, making it accessible on both desktop and mobile devices.
             </p>
           </li>
